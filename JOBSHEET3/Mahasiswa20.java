b205e1c3dd8152b83e57014b2170cf4feded93a5
mhmdzki <mhmdzki00@gmail.com>
2024-02-29 22:59:24 +0700

    Done3

---
+++ JOBSHEET3/Mahasiswa20.java
@@ -0,0 +1,9 @@
+package JOBSHEET3;
+
+public class Mahasiswa20 {
+    public String Nama, jenisKelamin;
+    public int Nim;
+    public double IPK;
+
+
+}
---
+++ JOBSHEET3/mainMhs20.java
@@ -0,0 +1,37 @@
+package JOBSHEET3;
+
+import java.util.Scanner;
+
+public class mainMhs20 {
+    public static void main(String[] args) {
+        Mahasiswa20[] Mhs = new Mahasiswa20[3];
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            Mhs[i] = new Mahasiswa20();
+            System.out.println("Masukkan data Mahasiswa ke- :" + (i+1));
+            System.out.print("Masukkan nama : ");
+            Mhs[i].Nama = sc.nextLine();
+            System.out.print("Masukkan NIM : ");
+            Mhs[i].Nim = sc.nextInt();
+
+            sc.nextLine();
+            
+            System.out.print("Masukkan jenis kelamin : ");
+            Mhs[i].jenisKelamin = sc.nextLine();
+            System.out.print("Masukkan IPK : ");
+            Mhs[i].IPK = sc.nextDouble();
+
+            sc.nextLine();
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("data Mahasiswa ke- " + (i+1));
+            System.out.println("Nama : " + Mhs[i].Nama);
+            System.out.println("Nim : " + Mhs[i].Nim);
+            System.out.println("Jenis kelamin : " + Mhs[i].jenisKelamin);
+            System.out.println("IPK : " + Mhs[i].IPK);
+        }
+    }
+}
