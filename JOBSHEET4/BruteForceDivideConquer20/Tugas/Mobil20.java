d051cc57ad4cd67e18697c09d257c4b6b1220d3a
mhmdzki <mhmdzki00@gmail.com>
2024-03-24 21:54:29 +0700

    Done4

---
+++ JOBSHEET4/BruteForceDivideConquer20/Tugas/Mobil20.java
@@ -0,0 +1,48 @@
+package JOBSHEET4.BruteForceDivideConquer20.Tugas;
+
+public class Mobil20 {
+    double variant, power[];
+    int accel[];
+
+    Mobil20(int variant) {
+        this.variant = variant;
+        this.power = new double[variant];
+        this.accel = new int[variant];
+    }
+
+    double rerata(double arr[]) {
+        double total = 0;
+        for (int i = 0; i < this.variant; i++) {
+            total += arr[i];
+        }
+        return total /= variant;
+    }
+
+    void accel(int arr[], int x, int y) {
+        if (x < y) {
+            int mid = partition(arr, x, y);
+            accel(arr, x, mid - 1);
+            accel(arr, mid + 1, y);
+        }
+    }
+
+    int partition(int[] arr, int x, int y) {
+        int highest = arr[y];
+        int i = x - 1;
+
+        for (int j = x; j < y; j++) {
+            if (arr[j] <= highest) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, y);
+        return i + 1;
+    }
+
+    void swap(int[] arr, int i, int j) {
+        int x = arr[i];
+        arr[i] = arr[j];
+        arr[j] = x;
+    }
+}
