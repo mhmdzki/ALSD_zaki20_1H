d98bca59baf46ee3a463cc62bf27f559aa6687f4
mhmdzki <mhmdzki00@gmail.com>
2024-05-20 19:09:45 +0700

    Done

--- Quiz2.java/Team.java
+++ Quiz2.java/Team.java
@@ -1,29 +1,17 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Team {
-
-    Team next;
-    String name;
+class Team {
+    String nama;
     int matchesPlayed;
     int wins;
-    int draws;
     int losses;
     int points;
-    List<Player> players;
+    Team next;
 
-    public Team(String name) {
-        this.name = name;
+    public Team(String nama) {
+        this.nama = nama;
         this.matchesPlayed = 0;
         this.wins = 0;
         this.losses = 0;
-        this.draws = 0;
         this.points = 0;
         this.next = null;
-        this.players = new ArrayList<>();
-    }
-
-    void addPlayer(Player player) {
-        players.add(player);
     }
 }
